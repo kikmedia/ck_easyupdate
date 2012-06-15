8245577e0fc0b5bf6d99bb576fd9fd1057fb930a
Carolina M. Koehn <office@kikmedia.de>
2012-06-15 10:01:49 +0200

    minor fixes

--- system/modules/easyupdate/SystemTL.php
+++ system/modules/easyupdate/SystemTL.php
@@ -5,7 +5,7 @@ if (!defined('TL_ROOT'))
  * TYPOlight webCMS
  * Copyright (C) 2005-2009 Leo Feyer
  *
- * This program is free software: you can redistribute it and/or
+ * This program is free software: you can redistribute it and/or 
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 2.1 of the License, or (at your option) any later version.
